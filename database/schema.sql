65a5204784b77a5cc8b382170de73d7816cb200b
Nhan Minh Truong <nhanminh.truong@ucf.edu>
2026-06-05 22:27:43 -0400

    Update schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,18 @@
+create schema lamp_project;
+Use lamp_project;
+
+Create table users(
+	user_id int primary key auto_increment not null unique,
+	username varchar(255) not null unique,
+	passwords varchar(255) not null,
+    user_priv int not null default 2
+);
+
+Create table contacts(
+	contact_id int primary key auto_increment not null unique,
+	reference_id int not null,
+	c_name varchar(255) not null,
+	phone varchar(20) unique,
+	email varchar(255) unique,
+	foreign key (reference_id) references users (user_id)
+);
